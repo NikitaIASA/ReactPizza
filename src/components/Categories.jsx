a1dec0254f2d55dfa52d8b0bf1d280ee38729014
fffounderrr <fffounderrr@gmail.com>
2022-10-07 06:11:57 +0300

    Start and routes

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(5);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li key={i} onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ""}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
